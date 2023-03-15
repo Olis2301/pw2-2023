6d354a4a1ff72e61c5dec9887d21bc0b3e6bce21
olisdecoy22@gmail.com <olisdecoy22@gmail.com_github>
2023-03-15 20:04:53 +0700

    Tugas 3

---
+++ PW2_TI12/pertemuan3/Tugas/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center py-3 bg-light mt-5">
+        <p>Created by <a href="#">Muhammad Nurcholis-TI12</a> &copy; <?php echo date('Y') ?></p>
+    </footer>
+</body>
+<link rel=¨stylesheet¨ type=¨text/css¨ href=¨[url_bootstrap_theme]¨>
+<link rel=¨stylesheet¨ href="css.css">
+</html>
---
+++ PW2_TI12/pertemuan3/Tugas/home.php
@@ -0,0 +1,14 @@
+<?php
+    include_once 'header.php';
+?>
+				<h1>Welcome Home!</h1>
+				<p>
+				Muhammad Nurcholis - 0110222200 - TI12
+                    <!--
+				        Tuliskan Nama - NIM - Rombel 
+				    -->
+				</p>
+
+<?php
+    include_once 'footer.php';
+?>
